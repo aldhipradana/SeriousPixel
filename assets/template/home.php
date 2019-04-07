cc6bdd2c59d437cb6dc12f9c303cd3a4423cd06d
Anthony Lee <anthonyleembahmo@gmail.com>
2019-04-07 23:51:05 +0800

    added row and column
    added row and column

--- assets/template/home.php
+++ assets/template/home.php
@@ -1,20 +1,38 @@
 <section class="IntroSection">
-    <h2>SERIOUS PIXEL</h2>
-    <p>“In Photography there is a reality so subtle that it becomes more real than
-        reality.”
-    </p>
-    <img src="assets/images/photo-1548765015-1e047ff5c9e9.jpg" alt="image 1" class="bw">
-    <img src="assets/images/photo-1484849559669-ceae3b12a437.jpg" alt="image 2" class="bw">
-    <img src="assets/images/photo-1519741414274-5b1ee71137fa.jpg" alt="image 3" class="bw">
-</section>
+    <div class="row">
+        <div class="column">
+            <h2>SERIOUS PIXEL</h2>
+            <p>“In Photography there is a reality so subtle that it becomes more real than
+                reality.”
+            </p>
+            <img
+                src="assets/images/photo-1548765015-1e047ff5c9e9.jpg"
+                alt="image 1"
+                class="bw">
+                <img
+                    src="assets/images/photo-1484849559669-ceae3b12a437.jpg"
+                    alt="image 2"
+                    class="bw">
+                    <img
+                        src="assets/images/photo-1519741414274-5b1ee71137fa.jpg"
+                        alt="image 3"
+                        class="bw"></div>
+                </div>
+            </section>
 
-<section class="IntroSection-2">
-    <p>“Taking an image, freezing a moment,
-        reveals how rich reality truly is”
-    </p>
-</section>
+            <section class="IntroSection-2">
+                <div class="row">
+                    <div class="column">
+                        <p>“Taking an image, freezing a moment, reveals how rich reality truly is”
+                        </p>
+                    </div>
+                </div>
+            </section>
 
-<section class="IntroSection-3">
-    <img src="assets/images/picture1.jpg" alt="image 4" class="bw">
-    <img src="assets/images/picture2.jpg" alt="image 5" class="bw">
-</section>
+            <section class="IntroSection-3">
+                <div class="row">
+                    <div class="column">
+                        <img src="assets/images/picture1.jpg" alt="image 4" class="bw">
+                            <img src="assets/images/picture2.jpg" alt="image 5" class="bw"></div>
+                        </div>
+                    </section>
