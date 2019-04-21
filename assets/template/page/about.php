ab869af591d90bf9d5351263d9b980261dfd0d6c
Anthony Lee <anthonyleembahmo@gmail.com>
2019-04-21 08:42:14 +0800

    initiate about page

---
+++ assets/template/page/about.php
@@ -0,0 +1,26 @@
+<section class="AboutSection">
+    <div class="row">
+        <div class="column">
+            <h2>About Us</h2>
+            <h3>Its about story, passion, emotion!</h3>
+            <p>Serious Pixel is the premium photo service that is offered a high end
+                appointment only service and we pride ourselves in care and detailed attention
+                we give to each of our clients
+            </p>
+        </div>
+    </div>
+</section>
+
+<section class="AboutSection-2">
+    <div class="row">
+        <div class="column">
+        
+        </div>
+    </div>
+</section>
+
+<section class="AboutSection-3">
+    <div class="row">
+        <div class="column">
+            </div>
+        </section>
