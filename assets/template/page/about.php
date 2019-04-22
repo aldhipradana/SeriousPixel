db2911f161052064503da305d619e3bcfa6435af
Anthony Lee <anthonyleembahmo@gmail.com>
2019-04-23 00:32:16 +0800

    push about dikit dulu, ngantuk gan ga kuat :v

--- assets/template/page/about.php
+++ assets/template/page/about.php
@@ -2,6 +2,7 @@
     <div class="row">
         <div class="column">
             <h2>About Us</h2>
+            <hr>
             <h3>Its about story, passion, emotion!</h3>
             <p>Serious Pixel is the premium photo service that is offered a high end
                 appointment only service and we pride ourselves in care and detailed attention
