88ff16b90e927026116fda8787891d9e7fbef522
Ari Wijaya <ariwijayaaaa@gmail.com>
2019-04-29 21:36:44 +0700

    Aku Nambahin Gallery, Homenya kyknya modif enter tok

--- assets/template/page/gallery.php
+++ assets/template/page/gallery.php
@@ -1,3 +1,89 @@
-<?php
+<style>
+    section.GallerySection{
+        background: #fff;
+        text-align: center;  
+    }
 
-echo" <center><p style=\"color:#000;\">its gallery </p></center>" ;
+    section.GallerySection .row-1 .column h2{
+        font-family:"Montserrat";
+        font-size: 30px;
+        padding : 30px 0 25px 0;
+        margin-block-start: unset;
+        margin-block-end: unset;
+        color: #d6b068;
+    }
+
+    section.GallerySection .row-1 .column p{
+        font-family:"HamilltonTwo";
+        font-size: 30px;
+        line-height: 1.3em;
+        letter-spacing: 1px;
+        color: #000;
+        max-width: 1000px;
+        margin-block-start: unset;
+        margin-block-end: unset;
+        margin: auto;
+    }
+
+    section.GallerySection-2{
+        background: #000;
+        text-align: center;
+        position: center;
+        
+    }
+
+    section.GallerySection-2 .row-1 {
+        background: #fff;
+        width: 900px;
+        margin : auto;
+        
+    }
+
+    section.GallerySection-2 .row-1 h3{
+        text-align: center;
+    }
+    
+    section.GallerySection-2 .row-1 .column.image{
+        background: url()
+    }
+
+    /* section.GallerySection-2 .row-1 .column img{
+        width: 100%;
+        padding: 20px 20px 100px 20px;
+    } */
+
+
+
+
+
+
+
+</style>
+
+<section class="GallerySection">
+    <div class="row-1">
+        <div class="column">
+            <h2>SERIOUS PIXEL WEDDING & PREWEDDING</h2>
+            <p>"When you realize you want to spend the rest of your life 
+                with somebody you want the rest of your life to start as soon as possible"</p>
+        </div>
+    </div>
+</section>
+
+<section class="GallerySection-2">
+    <div class="row-1">
+        <div class="column image">
+        </div>
+        <div class="column">
+            <h3>Bali Madira Hotel - May - Louise & Oscar</h3>
+        <p></p>
+        </div>
+        <!-- <div class="row-2">
+        <div class="column image">
+        </div>
+        <div class="column">
+            <h3>Bali Madira Hotel - May - Louise & Oscar</h3>
+        <p></p>
+        </div> -->
+    </div>
+</section>
