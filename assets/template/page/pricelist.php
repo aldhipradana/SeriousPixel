45d12058f07381663ee958e6c129ea1009f50a65
akselpratama <48973997+akselpratama@users.noreply.github.com>
2019-04-29 22:28:45 +0800

    Pricelist belum jadi

--- assets/template/page/pricelist.php
+++ assets/template/page/pricelist.php
@@ -1,3 +1,69 @@
-<?php
+<head>
+   
+    <style type="text/css">
+        @import url(/SeriousPixel/assets/css/pricelist.css);
+    </style>
 
-echo" <center><p style=\"color:#000;\">its price list </p></center>" ;
+</head>
+
+<section class="Sectionprice">
+    <div class="row">
+        <div class="column">
+
+         <!-- WEDDING    -->
+        <section class="Sectionprice-box">
+           <section class="Sectionprice-text h2">
+               <h2>WEDDING PHOTOGRAPHY PACKAGES</h2>
+               <div style="background-color:#3f4349; width:750px; height:2px; margin-top:30px; margin-left:115px"></div>
+               <div style="background-color:#9fa1a4; width:2px; height:150px; margin-top:87px; margin-left:495px"></div>
+            </section>
+           
+            <section class="Sectionprice-text h3">
+                   <h3>Wedding<br>Silver Package</h3>
+                   <div style="background-color:#d6b068; width:60px; height:4px; margin-top:10px; margin-left:240px"></div> 
+            </section>
+            <section class="Sectionprice-text p">
+                <p>Up to 3 Hours of Photography
+                    <br>2 Wedding Photographers
+                    <br>DVD of All Images
+                    <br>Maximum 300 Images
+                    </p>
+                    <p style="font-size:20pt; margin-top:10px">$599</p>
+                    <div style="background-color:#d6b068; width:110px; height:40px; margin-top:20px; margin-left:215px; padding-top:10px; font-family: Montserrat">Get Silver</div>
+            </section>
+            </section>
+
+            <section class="Sectionprice-text-2 h3">
+                   <h3>Wedding<br>Gold Package</h3>
+                   <div style="background-color:#d6b068; width:60px; height:4px; margin-top:10px; margin-left:192px"></div> 
+            </section>
+            <section class="Sectionprice-text-2 p">
+                <p>Up to 5 Hours of Photography
+                    <br>2 Wedding Photographers
+                    <br>DVD of All Images
+                    <br>Maximum 300 - 700 Images
+                    </p>
+                    <p style="font-size:20pt; margin-top:9px">$1199</p>
+                    <div style="background-color:#d6b068; width:110px; height:40px; margin-top:20px; margin-left:172px; padding-top:10px; font-family: Montserrat">Get Gold</div>
+
+            </section>
+            </section>
+            <!--  -->
+
+            <!-- FAMILY -->
+
+            <section class="Sectionprice-box-2">
+           <section class="Sectionprice-text-2 h2">
+               <h2>FAMILY PHOTOSHOOT PACKAGES</h2>
+               <div style="background-color:#3f4349; width:750px; height:2px; margin-top:30px; margin-left:115px"></div>
+               <div style="background-color:#9fa1a4; width:2px; height:150px; margin-top:87px; margin-left:495px"></div>
+            </section>
+            </section>
+
+
+
+            <!--  -->
+
+      </div>
+    </div>
+
