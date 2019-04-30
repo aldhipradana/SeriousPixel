b17609be21a35f3e9d32816b1efcb528761490ae
dedetirtamahayana <dedetirtamahayana@gmail.com>
2019-04-30 14:06:13 +0700

    dede

---
+++ assets/template/page/contact.php
@@ -0,0 +1,76 @@
+<section class="section-contact">
+    <div class="row">
+        <div class="column-1">
+        <h2>Contact Us</h2>
+            <h3> Get In Touch With Us</h3>
+                <p>We belive,that our passion and experience in this area is something that matters to
+                    make a beautiful artwork.But we also know that hight quality tools will elevate our works
+                    to the next level.
+                </p>
+                <p>In the end,We hope that through our works couples will watch,see,and feel their special moments
+                    in hight quality result.
+                </p>
+            <form action="">
+                <table>
+                    <tbody>
+                <tr>
+                    <td>
+                    <label for="inp" class="inp">
+                        <input type="text" id="inp" placeholder="&nbsp;">
+                        <span class="label">Your Name</span>
+                         <span class="border"></span>
+                    </label>
+                    <label for="inp" class="inp">
+                        <input type="text" id="inp" placeholder="&nbsp;">
+                        <span class="label">Booking Details</span>
+                         <span class="border"></span>
+                    </label>
+                    </td>
+                <tr>
+                    <td>
+                    <label for="inp" class="inp">
+                        <input type="text" id="inp" placeholder="&nbsp;">
+                        <span class="label">Email</span>
+                         <span class="border"></span>
+                    </label>
+                    <label for="inp" class="inp">
+                        <input type="textarea" id="inp" placeholder="&nbsp;">
+                        <span class="label">Your Message</span>
+                         <span class="border"></span>
+                    </label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                    <label for="inp" class="inp">
+                        <input type="text" id="inp" placeholder="&nbsp;">
+                        <span class="label">Phone</span>
+                         <span class="border"></span>
+                    </label>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="submit">
+                    <input type="submit" value="Submit Comment" name="post" id="comment-submit" class="button">
+                    </td>
+                </tr>
+                
+                 </tbody>    
+                 
+                </table>
+            </form>
+    </div>
+    <div class="column-2">
+        <h4>OUR ADDRES</h4>
+        <i class="fas fa-map-marker-alt">
+           <p>Jalan Batang Hari Gang 8 No.7A</p> 
+        </i>
+        <h4>OUR EMAIL</h4>
+        <i class="fas fa-envelope"></i>
+        <p>Seriouspixel@gmail.com</p> 
+        <h4>OUR PHONE</h4>
+        <i class="fab fa-whatsapp"></i>
+        <p>087862256245</p> 
+    </div>
+
+</section>
