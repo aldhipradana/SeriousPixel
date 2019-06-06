f1530727c78ac5e599df48e13da118132557c3d3
akselpratama <aksel.pratama06@gmail.com>
2019-06-06 20:37:26 +0800

    Update Pricelist

--- assets/template/page/pricelist.php
+++ assets/template/page/pricelist.php
@@ -1,8 +1,8 @@
 <section class="Pricesection">
-    <!-- WEDDING -->
+    
     <div class="row">
-
         <div class="column">
+            <!-- WEDDING -->
             <div class="box">
                 <h2>WEDDING PHOTOGRAPHY PACKAGE
                     <hr></h2>
@@ -11,41 +11,111 @@
                         <hr></h3>
                         <div class="content">
                             <p>Up to 3 Hours of Photography</p>
-                            <p>
-                                2 Wedding Photographers</p>
+                            <p>2 Wedding Photographers</p>
                             <p>DVD of All Images</p>
                             <p>Maximum 300 Images</p>
                             <p class="price">$599</p>
+                            <a href="#">Get Silver</a>
                         </div>
-                        <a href="#">Get Silver</a>
-                    </div>
-                </div>
 
-                <div class="column">
+                        <h3>Wedding
+                        <span>Gold Package</span>
+                        <hr></h3>
+                        <div class="content">
+                            <p>Up to 5 Hours of Photography</p>
+                            <p>2 Wedding Photographers</p>
+                            <p>DVD of All Images</p>
+                            <p>Maximum 300-700 Images</p>
+                            <p class="price">$1199</p>
+                            <a href="#">Get Gold</a>
+                        </div>   
+            </div>
+            <!-- FAMILY -->
+            <div class="box">
+                <h2>FAMILY PHOTOSHOOT PACKAGE
+                    <hr></h2>
+                    <h3>Family
+                        <span>Silver Package</span>
+                        <hr></h3>
+                        <div class="content">
+                            <p>Up to 2 Hours of Photography</p>
+                            <p>2 Professional Photographers</p>
+                            <p>Photo in 1 Location</p>
+                            <p>Maximum 300 Images</p>
+                            <p class="price">$399</p>
+                            <a href="#">Get Silver</a>
+                        </div>
+        
+                        <h3>Family
+                        <span>Gold Package</span>
+                        <hr></h3>
+                        <div class="content">
+                            <p>Up to 5 Hours of Photography</p>
+                            <p>2 Professional Photographers</p>
+                            <p>Photo in 2 Location</p>
+                            <p>Between 300 - 700 Images</p>
+                            <p class="price">$1099</p>
+                            <a href="#">Get Gold</a>
+                        </div>
+            </div>
+            <!-- BRANDING -->
+            <div class="box">
+                <h2>BRANDING PHOTOSHOOT PACKAGE
+                    <hr></h2>
+                    <h3>Branding
+                        <span>Silver Package</span>
+                        <hr></h3>
+                        <div class="content">
+                            <p>Consultation + brand strategy review</p>
+                            <p>On-location 90 min photoshoot</p>
+                            <p>Professional editing of best images</p>
+                            <p>Online gallery of 15 digital images</p>
+                            <p class="price">$699</p>
+                            <a href="#">Get Silver</a>
+                        </div>
+                        
+
+                        <h3>Branding
+                        <span>Gold Package</span>
+                        <hr></h3>
+                        <div class="content">
+                            <p>Consultation + brand strategy review</p>
+                            <p>On-location 4 hours photoshoot</p>
+                            <p>Professional editing of best images</p>
+                            <p>Online gallery of 25 digital images</p>
+                            <p class="price">$1499</p>
+                            <a href="#">Get Gold</a>
+                        </div>
+                        
+            </div>
+        </div>
+    </div>
+</section>
+                <!-- <div class="column">
                     <div class="box">
-                        <!-- DISINI ATUR JUDUL-JUDUL PACKAGE NYA -->
+                        *DISINI ATUR JUDUL-JUDUL PACKAGE NYA*
                         <h2>WEDDING PHOTOGRAPHY PACKAGE
                             <hr></h2>
-                            <!-- ATUR H3 NYA -->
+                            *ATUR H3 NYA*
                             <h3>Wedding
                                 <span>Silver Package</span>
                                 <hr></h3>
                                 <div class="content">
-                                    <!-- ATUR CONTENT -->
+                                    *ATUR CONTENT*
                                     <p>Up to 3 Hours of Photography</p>
                                     <p>
                                         2 Wedding Photographers</p>
                                     <p>DVD of All Images</p>
                                     <p>Maximum 300 Images</p>
-                                    <!-- ATUR HARGA -->
+                                    *ATUR HARGA*
                                     <p class="price">$599</p>
                                 </div>
-                                <!-- LINK -->
+                                *LINK*
                                 <a href="#">Get Silver</a>
                             </div>
                         </div>
 
-                    </div>
+                    </div> -->
 
                     <!-- <div style="background-color:#3f4349; width:750px; height:2px; margin:
                     auto"></div> -->
@@ -93,4 +163,4 @@
                     </div> -->
                     <!-- -->
 
-                </section>
+                
