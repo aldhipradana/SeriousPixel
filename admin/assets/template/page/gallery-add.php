f66de0ae57f09008a02a1609fd14f5a58bf5070c
Anthony Lee <anthonyleembahmo@gmail.com>
2019-06-13 14:42:12 +0800

    form add
    form add

--- admin/assets/template/page/gallery-add.php
+++ admin/assets/template/page/gallery-add.php
@@ -6,16 +6,25 @@
 <div class="column column-container">
     <div id="content">
         <div class="label-dashboard">
-            Gallery
+            Add Gallery
         </div>
 
         <div class="row content-dashboard">
             <div class="column">
-                <label></label>
-                <form action="#">
-
-
-                </form>
+                <div class="content-dashboard-container">
+                    <form action="#">
+                        <div class="form-group">
+                            <label class="label-form">Title</label>
+                            <input type="text" placeholder="Title" name="Title" class="input-form"></div>
+                        <div class="form-group">
+                            <label class="label-form">Description</label>
+                            <textarea name="Description" class="input-form" palaceholder="Description"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" value="Tambah Data">
+                        </div>
+                    </form>
+                </div>
             </div>
 
         </div>
